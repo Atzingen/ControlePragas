fc9c1d106faa84804e0c1e8802b6af4948e59580
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2019-06-23 01:22:22 -0300

    move sqlite to postgress

--- banco.py
+++ banco.py
@@ -1,72 +1,116 @@
 # -*- coding: latin-1 -*-
-import sqlite3, datetime
+import psycopg2, datetime, os
 
-local_banco = 'banco.db'
+host_ip = os.environ.get('db_ip_host')
+db_pwd = os.environ.get('db_pwd')
+
 
 def cria_banco():
-    conn = sqlite3.connect(local_banco)
-    cursor = conn.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS escorpiao(id	INTEGER PRIMARY KEY AUTOINCREMENT, latitude REAL NOT NULL, longitude REAL NOT NULL, time TEXT NOT NULL, quem TEXT NOT NULL, quantidade INTEGER)')
-    conn.commit()
-    cursor.execute('CREATE TABLE IF NOT EXISTS pessoas(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Nome TEXT, email TEXT NOT NULL UNIQUE, Quadra INTEGER, Lote INTEGER, Telefone TEXT,Senha TEXT)')
-    conn.commit()
-    conn.close()
-
+    try:
+        conn = psycopg2.connect(host=host_ip, database='postgres', user='postgres', password=db_pwd)
+        cursor = conn.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS escorpiao(id	SERIAL, latitude REAL NOT NULL, longitude REAL NOT NULL, time TEXT NOT NULL, quem TEXT NOT NULL, quantidade INTEGER)')
+        conn.commit()
+        cursor.execute('CREATE TABLE IF NOT EXISTS pessoas(id SERIAL, Nome TEXT, email TEXT NOT NULL UNIQUE, Quadra INTEGER, Lote INTEGER, Telefone TEXT,Senha TEXT)')
+        conn.commit()
+        conn.close()
+    except:
+        conn.close()
+        return None
+        
 def insere_pessoa(Nome, email, Quadra, Lote, Telefone, Senha):
-    conn = sqlite3.connect(local_banco)
-    cursor = conn.cursor()
-    cursor.execute("""INSERT INTO pessoas(Nome, email, Quadra, Lote, Telefone, Senha) 
-                VALUES(?,?,?,?,?,?)""",  (Nome, email, Quadra, Lote, Telefone, Senha))
-    conn.commit()
-    conn.close()
+    try:
+        conn = psycopg2.connect(host=host_ip, database='postgres', user='postgres', password=db_pwd)
+        cursor = conn.cursor()
+        cursor.execute("""INSERT INTO pessoas(Nome, email, Quadra, Lote, Telefone, Senha) 
+                    VALUES(%s,%s,%s,%s,%s,%s)""",  (Nome, email, Quadra, Lote, Telefone, Senha))
+        conn.commit()
+        conn.close()
+    except:
+        conn.close()
+        return None
 
 def delete_pessoa(email):
-    conn = sqlite3.connect(local_banco)
-    cursor = conn.cursor()
-    cursor.execute("""DELETE FROM pessoas WHERE email=?""", (email,))
-    conn.commit()
-    conn.close()
+    try:
+        conn = psycopg2.connect(host=host_ip, database='postgres', user='postgres', password=db_pwd)
+        cursor = conn.cursor()
+        cursor.execute("""DELETE FROM pessoas WHERE email=%s""", (email,))
+        conn.commit()
+        conn.close()
+    except:
+        conn.close()
+        return None
 
 def insere_praga(latitude, longitude, tempo, quem, quantidade):
-    conn = sqlite3.connect(local_banco)
-    cursor = conn.cursor()
-    cursor.execute("""INSERT INTO escorpiao(latitude, longitude, time, quem, quantidade) 
-                      VALUES(?,?,?,?,?)""",(latitude, longitude, tempo,quem, quantidade))
-    conn.commit()
-    conn.close()
+    try:
+        conn = psycopg2.connect(host=host_ip, database='postgres', user='postgres', password=db_pwd)
+        cursor = conn.cursor()
+        cursor.execute("""INSERT INTO escorpiao(latitude, longitude, time, quem, quantidade) 
+                          VALUES(%s,%s,%s,%s,%s)""",(latitude, longitude, tempo,quem, quantidade))
+        conn.commit()
+        conn.close()
+    except:
+        conn.close()
+        return None
 
 def lista_pragas(pessoa=None, data=None):
-    conn = sqlite3.connect(local_banco)
-    cursor = conn.cursor()
-    if pessoa:
-        if not data:
-            cursor.execute("""SELECT * FROM escorpiao WHERE quem=?""", (pessoa,))
+    try:
+        conn = psycopg2.connect(host=host_ip, database='postgres', user='postgres', password=db_pwd)
+        cursor = conn.cursor()
+        if pessoa:
+            if not data:
+                cursor.execute("""SELECT * FROM escorpiao WHERE quem=%s""", (pessoa,))
+            else:
+                pass  #TODO query by date
         else:
-            pass  #TODO query by date
-    else:
-        if not data:
-            cursor.execute("""SELECT * FROM escorpiao""")
-        else:
-            pass # aqui tambem query by date
-    return cursor.fetchall()
+            if not data:
+                cursor.execute("""SELECT * FROM escorpiao""")
+            else:
+                pass # aqui tambem query by date
+        return cursor.fetchall()
+    except:
+        conn.close()
+        return None
 
 def remove_praga(id):
-    conn = sqlite3.connect(local_banco)
-    cursor = conn.cursor()
-    cursor.execute("""DELETE FROM escorpiao WHERE id=?""", (id,))
-    conn.commit()
-    conn.close()
+    try:
+        conn = psycopg2.connect(host=host_ip, database='postgres', user='postgres', password=db_pwd)
+        cursor = conn.cursor()
+        cursor.execute("""DELETE FROM escorpiao WHERE id=%s""", (id,))
+        conn.commit()
+        conn.close()
+    except:
+        conn.close()
+        return None
 
 def senha_usuario(email):
-    conn = sqlite3.connect(local_banco)
-    cursor = conn.cursor()
-    cursor.execute("""SELECT senha from pessoas WHERE email=?""", (email,))
-    senha_banco = cursor.fetchone()[0]
-    return senha_banco
-
+    try:
+        conn = psycopg2.connect(host=host_ip, database='postgres', user='postgres', password=db_pwd)
+        cursor = conn.cursor()
+        cursor.execute("""SELECT senha from pessoas WHERE email=%s""", (email,))
+        senha_banco = cursor.fetchone()[0]
+        conn.close()
+        return senha_banco
+    except:
+        conn.close()
+        return None
+    
 def nome_usuario(email):
-    conn = sqlite3.connect(local_banco)
-    cursor = conn.cursor()
-    cursor.execute("""SELECT Nome from pessoas WHERE email=?""", (email,))
-    nome = cursor.fetchone()[0]
-    return nome
+    try:
+        conn = psycopg2.connect(host=host_ip, database='postgres', user='postgres', password=db_pwd)
+        cursor = conn.cursor()
+        cursor.execute("""SELECT Nome from pessoas WHERE email=%s""", (email,))    
+        nome = cursor.fetchone()[0]
+        conn.close()
+        return nome
+    except:
+        conn.close()
+        return None
+
+
+
+
+
+
+
+
